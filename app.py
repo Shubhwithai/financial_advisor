41ec20c47ea4047c741712793737cb81bd088b6b
Shubhwithai <shubham@buildfastwithai.com>
2024-06-26 22:37:43 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+import pathlib
+import textwrap
+import streamlit as st
+from PIL import Image
+import os
+
+import google.generativeai as genai
+
+from IPython.display import display
+from IPython.display import Markdown
+
+
+def to_markdown(text):
+    text = text.replace('•', '  *')
+    return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+
+model = genai.GenerativeModel(
+    'gemini-1.5-flash', system_instruction="You are a financial advisor. Do not answer anything except financial queries.")
+
+st.title('financial Advisor')
+
+genai.configure(api_key=os.getenviron("api_key"))
+
+
+uploaded_text = st.text_area("Enter your query here.")
+
+if st.button("Ask"):
+    response = model.generate_content([uploaded_text])
+    st.write(to_markdown(response.text).data)
